66ea56683053f1a8c9e87dcd3da759c1e6565a5f
vince523 <xjlufe@gmail.com>
2020-09-04 20:29:09 +0800

    add: 反转链表

---
+++ linked-list/reverseList/problem_24.go
@@ -0,0 +1,21 @@
+package problem_24
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+ }
+
+func ReverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	// 初始化头节点的前一个节点
+	var prev *ListNode = nil
+
+	for head != nil {
+		prev, head, head.Next = head, head.Next, prev
+	}
+
+	return prev
+}
---
+++ linked-list/reverseList/problem_24_test.go
@@ -0,0 +1,45 @@
+package problem_24
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestReverseList(t *testing.T) {
+	var	(
+		link1, link2, link3, link4 ListNode
+	)
+	link4 = ListNode{
+		Val:  4,
+		Next: nil,
+	}
+
+	link3 = ListNode{
+		Val:  3,
+		Next: &link4,
+	}
+
+	link2 = ListNode{
+		Val:  2,
+		Next: &link3,
+	}
+
+	link1 = ListNode{
+		Val:  1,
+		Next: &link2,
+	}
+
+	link := ListNode{
+		Val:  0,
+		Next: nil,
+	}
+
+	// nil
+	assert.Nil(t, ReverseList(nil))
+
+	// one node
+	assert.Equal(t, &link, ReverseList(&link))
+
+	// reverse
+	assert.Equal(t, 4, ReverseList(&link1).Val)
+}
